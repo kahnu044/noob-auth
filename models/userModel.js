fe61d98cb5ef012861197bf1287e47b651b9c761
kahnu044 <kahnucharan044@gmail.com>
2024-12-31 16:04:03 +0530

    user model updated with client schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,40 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
+// Define the ClientApp schema
+const clientAppSchema = new mongoose.Schema({
+    clientUrl: {
+        type: String,
+        required: true,
+    },
+    authType: {
+        type: String,
+        enum: ['google', 'password'],
+        required: true,
+    },
+    password: {
+        type: String,
+        required: function () {
+            return this.authType === 'password';
+        },
+    },
+});
+
+// Middleware to hash passwords for `clientApps`
+clientAppSchema.pre('save', async function (next) {
+    if (this.authType === 'password' && this.isModified('password')) {
+        try {
+            const salt = await bcrypt.genSalt(10);
+            this.password = await bcrypt.hash(this.password, salt);
+            next();
+        } catch (err) {
+            next(err);
+        }
+    } else {
+        next();
+    }
+});
+
 // Define the User schema
 const userSchema = new mongoose.Schema({
     email: {
@@ -23,42 +57,13 @@ const userSchema = new mongoose.Schema({
         unique: true,
         sparse: true,
     },
-    clientApps: [{
-        clientUrl: {
-            type: String,
-            required: true
-        },
-        authType: {
-            type: String,
-            enum: ['google', 'password'],
-            required: true
-        },
-        password: {
-            type: String,
-            required: function () {
-                return this.authType === 'password';
-            }
-        }
-    }],
-});
-
-// Password hashing middleware
-userSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
-
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (err) {
-        next(err);
-    }
+    clientApps: [clientAppSchema],
 });
 
 // Method to compare passwords for a specific client app
-userSchema.methods.comparePassword = async function(password, clientUrl) {
+userSchema.methods.comparePassword = async function (password, clientUrl) {
     const clientApp = this.clientApps.find(app => app.clientUrl === clientUrl);
-    if (!clientApp) return false;
+    if (!clientApp || clientApp.authType !== 'password') return false;
 
     return bcrypt.compare(password, clientApp.password);
 };
