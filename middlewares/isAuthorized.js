5a7bb61a111e5a5879e33c14c02e0e495175ca06
kahnu044 <kahnucharan044@gmail.com>
2025-01-01 18:26:28 +0530

    isAuthorized updated

--- middlewares/isAuthorized.js
+++ middlewares/isAuthorized.js
@@ -1,15 +1,22 @@
+const jwt = require("jsonwebtoken");
+
 const isAuthorized = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) {
-    return res.status(401).json({ status: false, message: "Unauthorized" });
+    return res.status(401).json({
+      status: false,
+      message: "Please provide authorization token in header",
+    });
   }
 
   try {
-    const decoded = jwt.verify(token, JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
-    return res.status(401).json({ status: false, message: "Unauthorized" });
+    return res
+      .status(401)
+      .json({ status: false, message: "Unauthorized access" });
   }
 };
 
